29deee3e74ab91b26a54c6170eb40914119ad538
3WW <basile.fouquet@epitech.eu>
2023-07-12 10:12:28 +0200

    [FIX] Trace parser

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import argparse, requests, os, sys, time, random
 from colorama import Fore
 from typing import Dict, List
-from requests import HTTPError
 
 ##config
 TestMandatorys: List[str] = ['compilation', 'tests']
@@ -10,8 +9,6 @@ class TraceParser:
     def __init__(self, Trace) -> None:
         self.winSize = os.get_terminal_size()
         self.Trace: Dict = Trace[len(Trace) - 1]
-        if not isinstance(Trace, List):
-            raise ValueError('Trace is not Valid')
         for mandatory in TestMandatorys:
             if not mandatory in  self.Trace.keys():
                 raise ValueError('Trace is not Valid')
@@ -59,12 +56,6 @@ group.add_argument("--result", action="store_true", help="Effectue l'action de r
 args = parser.parse_args()
 
 if args.register:
-    try:
-        TraceParser.shellPrint(requests.post(f'http://193.70.40.62:5000/register/{input("give your git username: ")}', timeout=10).json())
-    except Exception as e:
-        print('Error:', str(e))
+    TraceParser.shellPrint(requests.post(f'http://193.70.40.62:5000/register/{input("give your git username: ")}', timeout=10).json())
 elif args.result:
-    try:
-        TraceParser(requests.get(f'http://193.70.40.62:5000/mouli/{input("give your git username: ")}', timeout=10).json())
-    except Exception as e:
-        print('Error:', str(e))
+    TraceParser(requests.get(f'http://193.70.40.62:5000/mouli/{input("give your git username: ")}', timeout=10).json())
